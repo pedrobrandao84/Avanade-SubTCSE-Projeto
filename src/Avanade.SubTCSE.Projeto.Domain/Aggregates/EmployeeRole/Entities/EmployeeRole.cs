2fd5874b56d7bc1e5403ccf29b66bb8b2ededde3
Pedro Costa Brandao <pedro.costa.brandao@avanade.com>
2021-06-23 20:25:33 -0300

    Update project

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
@@ -0,0 +1,18 @@
+﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Entities
+{
+    public record EmployeeRole : BaseEntity<string>
+    {
+        public EmployeeRole(string id, string roleName)
+        {
+            Id = id;
+            RoleName = roleName;
+        }
+
+        public EmployeeRole(string roleName)
+        {
+            RoleName = roleName;
+        }
+
+        public string RoleName { get; init; }
+    }
+}
